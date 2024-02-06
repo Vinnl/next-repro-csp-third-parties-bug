74ab3b38ae88770105f90cd30ebfb42ec9c9222d
Vincent <git@vincenttunru.com>
2024-02-06 12:46:29 +0100

    Demonstrate CSP bug
    Third-party scripts (like GTM) don't get the nonce added.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
+import { GoogleTagManager } from '@next/third-parties/google'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <GoogleTagManager gtmId={process.env.GA_TOKEN}/>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -4,5 +4,7 @@ import Script from "next/script";
 export default function Page() {
   const nonce = headers().get("x-nonce");
 
-  return <Script src="https://..." strategy="afterInteractive" nonce={nonce} />;
+  return <>
+    Page
+  </>;
 }
