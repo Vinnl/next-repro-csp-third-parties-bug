e5f028574b76cfc73d2f1c5c15ff35af1bc363d5
Vincent <git@vincenttunru.com>
2024-02-06 15:51:45 +0100

    Fork @next/third-parties to allow nonce

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,13 @@
-import { GoogleTagManager } from '@next/third-parties/google'
+import { headers } from 'next/headers'
+import { GoogleTagManager } from './GoogleTagManager'
 
 export default function RootLayout({ children }) {
+  const nonce = headers().get('x-nonce')
+
   return (
     <html lang="en">
       <body>{children}</body>
-      <GoogleTagManager gtmId={process.env.GA_TOKEN}/>
+      <GoogleTagManager gtmId={process.env.GA_TOKEN} nonce={nonce}/>
     </html>
   );
 }
